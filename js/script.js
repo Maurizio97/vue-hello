3b23e9d2d7b74ba33f22db37372ff6ab887b2730
Maurizio <proiettomaurizio97@gmail.com>
2021-11-10 15:01:52 +0100

    es complete

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#container',
+    data: {
+        text : "Bella questa documentazione......",
+        // image : "https://mole24.it/wp-content/uploads/2013/11/giphoscope-6.gif"
+        image : "https://forums-images.oneplus.net/data/webimg/2021/01-29/601395233bfbf.gif"
+    }
+})
